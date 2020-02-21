3f3188367dcd729a8d6b1a83d6b7e5ffe4ca71d7
fritzgt <tuanis@live.com>
2020-02-21 10:46:24 +0100

    added toolbar with fontawesome

--- Minimalist Unit Converter/ViewController.swift
+++ Minimalist Unit Converter/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import FontAwesome_swift
 
 class ViewController: UIViewController {
+    
+    //MARK: - Properties
+//    let attributes = [NSAttributedStringKey.font:UIFont.fonta ]
+    
+    //MARK: - Outlets
+    @IBOutlet weak var distanceButton: UIBarButtonItem!
+    @IBOutlet weak var temperatureButton: UIBarButtonItem!
+    @IBOutlet weak var lengthButton: UIBarButtonItem!
+    @IBOutlet weak var volumenButton: UIBarButtonItem!
+    @IBOutlet weak var weightButton: UIBarButtonItem!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupToolBar()
+      
+    }
+    
+    //MARK: - Setup
+    func setupToolBar() {
+        
+          distanceButton.image = UIImage.fontAwesomeIcon(name: .tachometerAlt , style: .solid, textColor: UIColor.blue, size: CGSize(width: 30, height: 30))
+        temperatureButton.image = UIImage.fontAwesomeIcon(name: .thermometerHalf , style: .solid, textColor: UIColor.blue, size: CGSize(width: 30, height: 30))
+        
+        lengthButton.image = UIImage.fontAwesomeIcon(name: .ruler , style: .solid, textColor: UIColor.blue, size: CGSize(width: 30, height: 30))
+        
+        volumenButton.image = UIImage.fontAwesomeIcon(name: .tint , style: .solid, textColor: UIColor.blue, size: CGSize(width: 30, height: 30))
+        
+        weightButton.image = UIImage.fontAwesomeIcon(name: .balanceScaleRight , style: .solid, textColor: UIColor.blue, size: CGSize(width: 30, height: 30))
+        
+        
     }
 
 
