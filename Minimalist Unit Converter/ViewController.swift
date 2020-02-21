675b27166094ef326d949dd201dcca73adb267ed
fritzgt <tuanis@live.com>
2020-02-21 06:42:56 +0100

    Initial Commit

---
+++ Minimalist Unit Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Minimalist Unit Converter
+//
+//  Created by FGT MAC on 2/21/20.
+//  Copyright © 2020 FGT MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
